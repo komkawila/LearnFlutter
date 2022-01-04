330785078f53f3e85147f039c0753fc3694f5eeb
komkawila <kompanuwat@gmail.com>
2022-01-04 20:12:06 +0700

    first commit

---
+++ blekomkawila15/android/app/src/main/kotlin/com/example/blekomkawila15/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.blekomkawila15
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ komkawila1/android/app/src/main/kotlin/com/example/komkawila1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.komkawila1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
